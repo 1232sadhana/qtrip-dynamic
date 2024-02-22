8213be6b33fa520f74b06fa4f7d695373e70ba93
SADHANA JADHAV <136025180+1232sadhana@users.noreply.github.com>
2024-02-22 21:58:39 +0530

    Update index.js

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://65.0.40.89:8082" };
+const config = { backendEndpoint: "https://crio-1232sadhana-me-qkart-frontend-v2.onrender.com" };
 
 export default config;
